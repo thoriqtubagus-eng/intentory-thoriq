1bab59da2ef6f88dc8be29e08cf3a3fc85c91a0e
ruu <mirin19778@gmail.com>
2026-09-05 16:19:53 +0700

    perf: remove channel_binding, cache PrismaClient globally for faster cold starts

--- api/_lib/prisma.js
+++ api/_lib/prisma.js
@@ -1,14 +1,15 @@
 const { PrismaClient } = require("@prisma/client");
 
-let prisma;
+const globalForPrisma = globalThis;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
 
 module.exports = { prisma };
