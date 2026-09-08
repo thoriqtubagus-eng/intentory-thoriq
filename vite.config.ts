757ad689983039cf51fe5095592a36b65ea4621c
toriq <thoriqtubagus@gmail.com>
2026-09-08 16:40:59 +0700

    Fix vite port to use dynamic PORT env

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: parseInt(process.env.PORT || "8080"),
+    strictPort: false,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
